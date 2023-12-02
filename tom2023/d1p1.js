04a701841ef61304e03c808347b40258b7f56fd7
tomangioletti <tomangioletti@gmail.com>
2023-12-01 23:47:35 -0500

    d1p1 done

---
+++ tom2023/d1p1.js
@@ -0,0 +1,14 @@
+function log(s){console.log(s)}
+function isd(c){return((c>='0')&&(c<='9'))} 
+function itf(a,f){let v={val:0}
+  for(let i=0;i<a.length;i++)if(!f(a[i],v))break;
+  return v}
+function fd(s){}
+
+const fs=require('node:fs')
+let da=fs.readFileSync('d1p1','utf8').split("\n").filter(x=>x.trim().length>0)
+da=da.map(x=>x.split('').filter(isd).join(''))
+
+let sum=itf(da, (x,v)=>{log('d:'+x+' x[0]:'+x[0]);v.val+=parseInt(x[0]+x[x.length-1]);return true}).val
+log(sum)
+
