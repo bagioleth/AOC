1374bf1d5151c24cf8e3f44041b313c2c9b3287d
tomangioletti <tomangioletti@gmail.com>
2023-12-08 12:45:10 -0500

    d3p1 cleanup.

--- tom2023/d3p1.js
+++ tom2023/d3p1.js
@@ -1,6 +1,9 @@
 function log(s){console.log(s)}
 function isd(c){return((c>='0')&&(c<='9'))}
-function g(r,c){try{return da[r][c]}catch(e){return '.'}}
+function g(r,c){
+  if(r<0||r>=da.length)return '.'
+  if(c<0||c>=da[r].length)return '.'
+  return da[r][c]}
 function isq(x){return x!='.'&&!isd(x)}
 function lq(r,c){return (
 isq(g(r-1,c-1))||isq(g(r-1,c))||isq(g(r-1,c+1))||
@@ -8,7 +11,7 @@ isq(g(r,c-1))  ||               isq(g(r,c+1))||
 isq(g(r+1,c-1))||isq(g(r+1,c))||isq(g(r+1,c+1))
 )}
 
-const fs=require('node:fs')
+const fs=require('fs')
 let da=fs.readFileSync('d3p1','utf8').split("\n").filter(x=>x.trim().length>0)
 da=da.map(e=>e.split(''))
 
@@ -17,16 +20,13 @@ for(let r=0;r<da.length;r++){
   let ns='', q=false
   for(let c=0;c<da[r].length;c++){
     let x=g(r,c)
-    log('r='+r+' c='+c+' x='+x+' q='+q+' ns='+ns)
     if(isd(x)){ns+=x;q=q||lq(r,c)}
     else{
-      if(q){sum+=Number.parseInt(ns,10);log(ns);log(sum)}
+      if(q){sum+=parseInt(ns)}
       ns=''
       q=false}}
-  if(q){sum+=Number.parseInt(ns,10);log(ns);log(sum)}
+  if(q){sum+=parseInt(ns)}
 }
-
-
 
 log(sum)
 
