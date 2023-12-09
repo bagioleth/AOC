83ce472c365db70af07eda6f93289a63b48943e4
tomangioletti <tomangioletti@gmail.com>
2023-12-09 08:58:57 -0500

    working on d3p2

---
+++ tom2023/d3p2.js
@@ -0,0 +1,35 @@
+function log(s){console.log(s)}
+function isd(c){return((c>='0')&&(c<='9'))}
+function g(r,c){
+  if(r<0||r>=da.length)return '.'
+  if(c<0||c>=da[r].length)return '.'
+  return da[r][c]}
+function isq(x){return x!='.'&&!isd(x)}
+function lq(r,c){return (
+isq(g(r-1,c-1))||isq(g(r-1,c))||isq(g(r-1,c+1))||
+isq(g(r,c-1))  ||               isq(g(r,c+1))||
+isq(g(r+1,c-1))||isq(g(r+1,c))||isq(g(r+1,c+1))
+)}
+
+const fs=require('fs')
+let da=fs.readFileSync('d3p2','utf8').split("\n").filter(x=>x.trim().length>0)
+//da=da.map(e=>e.split('')).map(e=>!isq(e)?e:(e=='*'?e:'.'))
+da=da.map(e=>e.split('').map(e=>!isq(e)?e:(e=='*'?e:'.')))
+
+let sum=0
+for(let r=0;r<da.length;r++){
+  let ns='', q=false, ls=''
+  for(let c=0;c<da[r].length;c++){
+    ls+=(g(r,c))
+    let x=g(r,c)
+    if(isd(x)){ns+=x;q=q||lq(r,c)}
+    else{
+      if(q){sum+=parseInt(ns)}
+      ns=''
+      q=false}}
+  if(q){sum+=parseInt(ns)}
+  log(ls)
+}
+
+log(sum)
+
